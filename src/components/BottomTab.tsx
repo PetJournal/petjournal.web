1b99a7710ce8f310f5921b2a2ebbfc7e92849596
Larissa Oliveira <oliveira.larissa.dv@gmail.com>
2023-06-01 13:40:54 -0300

    feat: add icons

--- src/components/BottomTab.tsx
+++ src/components/BottomTab.tsx
@@ -1,30 +1,37 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
+import homeIcon from '../assets/svg/home.svg';
+import petsIcon from '../assets/svg/pets.svg';
+import tutorIcon from '../assets/svg/tutor.svg';
 
 function BottomTab() {
   const { pathname, push } = useRouter();
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 flex justify-around bg-custom-purple">
+    <nav className="fixed bottom-0 left-0 right-0 flex justify-between p-2 px-8 bg-custom-purple">
       <button
         onClick={() => push('/')}
-        className={`p-4 ${
+        className={`flex flex-col items-center ${
           pathname === '/' ? 'text-custom-blue' : 'text-white'
         }`}
       >
+        <Image src={homeIcon} alt="" />
         Home
       </button>
       <button
-        className={`p-4 ${
+        className={`flex flex-col items-center ${
           pathname === '/pets' ? 'text-custom-blue' : 'text-white'
         }`}
       >
+        <Image src={petsIcon} alt="" />
         Pets
       </button>
       <button
-        className={`p-4 ${
+        className={`flex flex-col items-center ${
           pathname === '/tutor' ? 'text-custom-blue' : 'text-white'
         }`}
       >
+        <Image src={tutorIcon} alt="" />
         Tutor
       </button>
     </nav>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,21 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import menuIcon from '../assets/svg/menu-burger.svg';
+import calendarIcon from '../assets/svg/calendar.svg';
+import searchIcon from '../assets/svg/search.svg';
+import vaccineIcon from '../assets/svg/vaccine.svg';
+import medicalDropIcon from '../assets/svg/medical-drops.svg';
+import Image from 'next/image';
 
 function Home() {
   return (
     <div className="p-4">
       <header className="flex justify-between mb-5">
         <h2>Olá, Camila!</h2>
-        <button>menu</button>
+        <button>
+          <Image src={menuIcon} alt="" />
+        </button>
       </header>
 
       <Swiper
@@ -30,20 +38,20 @@ function Home() {
         </div>
 
         <div className="grid grid-cols-2 gap-3 mt-2 text-white">
-          <button className="flex items-center justify-center p-4 text-center rounded-lg bg-custom-blue h-36">
-            {/* image */}
+          <button className="flex flex-col gap-3 h-[150px] items-center justify-center p-4 py-5 text-center rounded-lg bg-custom-blue">
+            <Image src={calendarIcon} alt="" width={80} height={80} />
             <p className="font-medium">Agenda</p>
           </button>
-          <button className="flex items-center justify-center p-4 text-center rounded-lg bg-custom-purple h-36">
-            {/* image */}
+          <button className="flex flex-col gap-3 h-[150px] items-center justify-center p-4 py-5 text-center rounded-lg bg-custom-purple">
+            <Image src={searchIcon} alt="" width={80} height={80} />
             <p className="font-medium">Localizar seviços</p>
           </button>
-          <button className="flex items-center justify-center p-4 text-center rounded-lg bg-custom-cyan h-36">
-            {/* image */}
+          <button className="flex flex-col gap-3 h-[150px] items-center justify-center p-4 py-5 text-center rounded-lg bg-custom-cyan">
+            <Image src={vaccineIcon} alt="" width={80} height={80} />
             <p className="font-medium">Registro de Vacinas</p>
           </button>
-          <button className="flex items-center justify-center p-4 text-center rounded-lg bg-custom-pink h-36">
-            {/* image */}
+          <button className="flex flex-col gap-[2px] h-[150px] items-center justify-center p-4 py-5 text-center rounded-lg bg-custom-pink">
+            <Image src={medicalDropIcon} alt="" width={80} height={80} />
             <p className="font-medium">Registro de Vermifugos</p>
           </button>
         </div>
