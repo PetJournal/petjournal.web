067246408d73e2836b58390cfd62ddeac1840cf7
brenomorp <brenomoreirapro@gmail.com>
2023-07-30 15:43:23 -0300

    chore: cofigure tailwind.config.js

--- src/pages/pet-register/species.tsx
+++ src/pages/pet-register/species.tsx
@@ -1,6 +1,13 @@
 import SpeciesCard from '@/components/SpeciesCard';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { Fredoka } from '@next/font/google';
+
+const fredoka = Fredoka({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  variable: '--font-fredoka',
+});
 
 function Species() {
   const [isInputActive, setIsInputActive] = useState<boolean>(false);
@@ -21,13 +28,13 @@ function Species() {
   ];
 
   return (
-    <>
+    <div className={`${fredoka.variable} font-sans`}>
       <header className="flex items-center justify-center h-14 font-normal text-base shadow-[0_2px_4px_0_rgba(0,0,0,0.06)] mb-9">
-        <h1 className="text-[#9A0963]">Cadastro Pet</h1>
+        <h1 className="text-primary-400">Cadastro Pet</h1>
       </header>
       <main className="max-w-sm mx-auto flex flex-col items-center">
-        <h2 className="text-[#9A0963] text-center text-[22px] leading-6 px-2">
-          Olá <span className="text-[#E06197]">Fulano</span>, gostaríamos de
+        <h2 className="text-primary-400 text-center text-[22px] leading-6 px-2">
+          Olá <span className="text-primary-300">Fulano</span>, gostaríamos de
           saber qual a espécie do seu Pet:
         </h2>
 
@@ -44,7 +51,7 @@ function Species() {
         </div>
 
         <button
-          className="bg-[#54C1E9] w-[200px] py-2 rounded-3xl mt-5 text-white hover:bg-[#77DAF4] focus:bg-[#77DAF4] transition-colors"
+          className="bg-link-400 w-[200px] py-2 rounded-3xl mt-5 text-gray-100 hover:bg-link-300 focus:bg-link-300 transition-colors"
           onClick={() => {
             setIsInputActive(!isInputActive);
             setSelectedSpecies('');
@@ -55,14 +62,17 @@ function Species() {
 
         {isInputActive && (
           <div className="mt-10">
-            <label htmlFor="species" className="self-start ml-3 text-[#9A0963]">
+            <label
+              htmlFor="species"
+              className="self-start ml-3 text-primary-400"
+            >
               Insira a espécie:
             </label>
             <input
               type="text"
               name="species"
               placeholder="Digite aqui..."
-              className="border-2 border-[#B2B2B2] rounded-lg px-3 py-2 w-full outline-none"
+              className="border-2 border-gray-300 rounded-lg px-3 py-2 w-full outline-none"
               value={selectedSpecies}
               onChange={(e) => setSelectedSpecies(e.target.value)}
             />
@@ -72,19 +82,19 @@ function Species() {
         <div className="absolute bottom-8 flex gap-4">
           <Link
             href="/pet-register"
-            className="text-[#9A0963] border-2 border-[#9A0963] rounded-3xl w-40 py-2 text-center"
+            className="text-primary-400 border-2 border-primary-400 rounded-3xl w-40 py-2 text-center"
           >
             Voltar
           </Link>
           <button
             disabled={isContinueDisabled}
-            className="bg-[#9A0963] text-white rounded-3xl w-40 py-2 disabled:text-[#B2B2B2] disabled:border-2 disabled:border-[#B2B2B2] disabled:bg-transparent"
+            className="bg-primary-400 text-gray-100 rounded-3xl w-40 py-2 disabled:text-gray-300 disabled:border-2 disabled:border-gray-300 disabled:bg-transparent"
           >
             Continuar
           </button>
         </div>
       </main>
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-fredoka)'],
+      },
+      colors: {
+        primary: {
+          300: '#E06197',
+          400: '#9A0963',
+        },
+        gray: {
+          100: '#F2F2F2',
+          300: '#B2B2B2',
+        },
+        link: {
+          300: '#77DAF4',
+          400: '#54C1E9',
+        },
+      },
+    },
   },
   plugins: [],
 };
