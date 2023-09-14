95101f4bba8f1d4745cf04d349d8d66df32c8449
brenomorp <brenomoreirapro@gmail.com>
2023-09-14 19:29:19 -0300

    chore: remove miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 
-// Mock API
-import buildServer from '@/server/mirage';
-
-buildServer();
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
